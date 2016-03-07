8166db711a6f4e2bc670910944ccf63b95076957
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-07 11:24:12 -0800

    create User class with create, read, and delete methods

---
+++ src/main/java/User.java
@@ -0,0 +1,73 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class User {
+  private int id;
+  private String name;
+  private String password;
+
+  public User (String name, String password) {
+    this.name = name;
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public static List<User> all() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM users";
+      return con.createQuery(sql).executeAndFetch(User.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherUser) {
+    if(!(otherUser instanceof User)) {
+      return false;
+    } else {
+      User newUser = (User) otherUser;
+      return this.getName().equals(newUser.getName());
+    }
+  }
+
+  public void save() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("password", this.password)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static User find(int id) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM users WHERE id = :id;";
+      User user = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(User.class);
+      return user;
+    }
+  }
+
+  public void delete() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM users WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,63 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.List;
+
+public class UserTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void user_instantiates_correctly_true() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    assertTrue(testUser instanceof User);
+  }
+
+  @Test
+  public void getName_returnsName_string() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    assertEquals("Michelle", testUser.getName());
+  }
+
+  @Test
+  public void getPassword_returnsPassword_string() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    assertEquals("p@$$w0rd", testUser.getPassword());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(User.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfUsersAreTheSame() {
+    User testUser1 = new User("Michelle", "p@$$w0rd");
+    User testUser2 = new User("Michelle", "p@$$w0rd");
+    assertTrue(testUser1.equals(testUser2));
+  }
+
+  @Test
+  public void saves_savesUserToDatabase() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    testUser.save();
+    assertTrue(User.all().contains(testUser));
+  }
+
+  @Test
+  public void find_returnsUserFromDatabase() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    testUser.save();
+    assertEquals(testUser, User.find(testUser.getId()));
+  }
+
+  @Test
+  public void delete_deletesUserFromDatabase() {
+    User testUser = new User("Michelle", "p@$$w0rd");
+    testUser.save();
+    testUser.delete();
+    assertFalse(User.all().contains(testUser));
+  }
+
+}
