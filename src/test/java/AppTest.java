decfcb680c2ad1cb8791ec423cf3ae1ee98e0bc9
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-07 13:39:58 -0800

    add routes for home and login page

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
   public WebDriver getDefaultDriver() {
-      return webDriver;
+    return webDriver;
   }
 
   @ClassRule
@@ -18,7 +20,14 @@ public class AppTest extends FluentTest {
 
   @Test
   public void rootTest() {
-      goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("");
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Portland Permaculture Project");
+  }
+
+  @Test
+  public void login_loadsFromHomepage() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Login"));
+    assertThat(pageSource()).contains("Returning User");
   }
 }
